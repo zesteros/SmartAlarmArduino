945bb5f12bedb0afc842107847122f4055873c8f
zesteros <angelo_384@hotmail.com>
2018-01-13 17:48:42 -0600

    VARS declared

--- AlarmDroid.ino
+++ AlarmDroid.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+/*
+  8->rxbt
+  9->txbt
+  4->pir
+  5->relay
+*/
+const int rxbt = 8;
+const int txbt = 9;
+const int pirPin = 4;
+const int relayPin = 5;
+SoftwareSerial bt(rxbt, txbt); //tx,rx
+
+void setup() {
+  Serial.begin(9600);
+  bt.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
